a2904856feb0bc1c6384c5486bdb5c3ac193cbb6
Shakhruz Khaldarbekov <shakhruzkhaldarbekov@gmail.com>
2024-11-03 17:37:51 +0500

    done

---
+++ js/script.js
@@ -0,0 +1,42 @@
+$(function() {
+    let helpModal = $(".modal-help");
+    let sentModal = $(".modal-sent");
+    let activeButton = $(".act-btn");
+    let modalHelpClose = $(".modal-help-close");
+    let input = $(".inp");
+    let form = $(".modal-help form");
+    activeButton.on("click", () => {
+        helpModal.css("display", "block")
+    })
+    modalHelpClose.on("click", () => {
+        helpModal.css("display", "none")
+    })
+    input.on("change", () => {
+        input.each(function () {
+            if ($(this).val().length == 0) {
+                $(this).addClass("required");
+            } else {
+                $(this).removeClass("required");
+            }
+        })
+    })
+    form.on("submit", e => {
+        let full = true;
+        $(".inp").each(function() {
+            if ($(this).val().length == 0) {
+                full = false;
+            }
+        })
+        e.preventDefault();
+
+        if (!full) {
+            return;
+        }
+        helpModal.css("display", "none");
+        sentModal.css("display", "block");
+
+        setTimeout(() => {
+            sentModal.css("display", "none");
+        }, 3000);
+    })
+})
